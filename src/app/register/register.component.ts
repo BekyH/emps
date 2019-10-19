f3ebf87e6b0efeafb6b6631b3b9ecb77bca3b987
unknown <bereketheramo25@gmail.com>
2019-10-19 23:42:05 +0300

    modification of form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder,FormGroup} from '@angular/forms';
+import {user,genderType} from '../../../user';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  userForm:FormGroup;
+  User:user;
+  gendertype=genderType;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) 
+  {
+    this.createForm;
+   }
 
   ngOnInit() {
+  }
+  createForm(){
+    this.userForm = this.fb.group({
+      firstname:'',
+      lastname:'',
+      email:'',
+      city:'',
+      phoneno:'',
+      gendertype:'male'
+    });
+  }
+  onSubmit(){
+    this.User = this.userForm.value;
+    console.log(user);
+    this.userForm.reset();
   }
 
 }
