0cc0298f15adb1f421dd2f7fa79ffdc4ac867ca7
unknown <bereketheramo25@gmail.com>
2019-10-24 19:39:45 +0300

    home page modified

---
+++ src/app/company.ts
@@ -0,0 +1,7 @@
+export class Company{
+    name:String;
+    phoneno:String;
+    email:String;
+    city:String;
+    cateogry:string;
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
+import {Company} from '../company';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  companyForm:FormGroup;
+  company:Company;
+  constructor(private fb:FormBuilder) {
+    this.createForm();
+   }
+   
   ngOnInit() {
+  }
+  createForm(){
+    this.companyForm = this.fb.group({
+      name:['',Validators.required],
+      phoneno:['',Validators.required],
+      email:['',Validators.required],
+      city:['',Validators.required],
+      category:['',Validators.required]
+    })
   }
 
 }
